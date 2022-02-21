801c94b5d1bde78ec28ea7fd3e1bda6a0e035a0a
Lauraalinda <lauraallindaa@gmail.com>
2022-02-21 14:28:53 +0300

    my first kotlin assignment

---
+++ src/kott1.kt
@@ -0,0 +1,30 @@
+fun main (){
+    var names=arrayOf<String>("lAURA ALINDA" , "LONA CRYSTABEL" , "KENYANA LINDA")
+    var age=arrayOf<Int>(20, 23, 31)
+    var phoneNumber=arrayOf<String>("072344822", "0772434325", "072345355")
+    var weight=arrayOf<String>("120kg", "98kg", "45kg")
+    val isKenyan:Boolean=true
+    val notKenyan:Boolean=false
+
+    //the names are organised from the first to the last//
+    println(names[0])
+    println(age[0])
+    println(phoneNumber[0])
+    println(weight[0])
+    println(notKenyan)
+
+
+    println(names[1])
+    println(age[1])
+    println(phoneNumber[1])
+    println(weight[1])
+    println(isKenyan)
+
+
+    println(names[2])
+    println(age[2])
+    println(phoneNumber[2])
+    println(weight[2])
+    println(isKenyan)
+
+}
